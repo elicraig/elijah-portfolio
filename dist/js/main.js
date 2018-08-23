417a0e1c153083ef6f6738810696b5a2b7408340
Elijah Craig <elijahlcraig@gmail.com>
2018-08-23 13:20:55 -0500

    Menu Overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -27,5 +27,7 @@ function toggleMenu() {
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
+    //Set Menu State
+    showMenu = false;
   }
 }
